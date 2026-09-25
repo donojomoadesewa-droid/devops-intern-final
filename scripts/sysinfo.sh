98c6f6038055e2855e4d13eccb1bafc98f2821dc
donojomoadesewa-droid <donojomoadesewa@gmail.com>
2026-09-25 08:22:18 +0100

    fix: correct ISO-8601 date format in sysinfo.sh

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -5,7 +5,7 @@ echo "User: $(whoami)"
 echo "UID: $(id -u)"
 echo "Hostname: $(hostname)"
 echo "Kernel: $(uname -r)"
-echo "Date: $(date -u +%Y-%m-%dT%H:%M:"$SZ")"
+echo "Date: $(date -u +%Y-%m-%dT%H:%M:%S%z)"
 echo "Disk Usage"
 df -h
 echo "Memory Usage"
